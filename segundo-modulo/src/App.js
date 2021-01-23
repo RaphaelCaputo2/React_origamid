df9ae5e41ff5c9ad3c50c39f4e76e689a4c89f39
Raphael Caputo <faelelf2@gmail.com>
2021-01-23 20:01:32 -0300

    'Enviando restante da aplicacao'

--- segundo-modulo/src/App.js
+++ segundo-modulo/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+
+},
+{
+  id: 2,
+  nome: 'Notebook',
+  preco: 'R$ 3000',
+  cores: ['#ffd045', '#d4394b', '#f37c59'],
+
+},
+{
+  id: 3,
+  nome: 'tablet',
+  preco: 'R$ 1500',
+  cores: ['#365069', '#47c1c8', '#f95786'],
+
+},
+];
+
 
 function App() {
+  const data = produtos.filter(({preco}) => (
+    Number(preco.replace('R$ ', '') > 1500)
+  ));
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <section>
+    {data.map(({nome, preco, cores, id}) => (
+      <div key={id}>
+     <h1>{nome}</h1>
+     <p>Preço: {preco}</p>
+     <ul>
+     <p>{cores.map((cor) => (
+<li style={{backgroundColor:cor, color:'white', maxWidth:100}}key={cor}>{cor}</li>
+     ))}</p>
+     </ul>
+     </div>
+    ))}
+ </section>
   );
 }
 
